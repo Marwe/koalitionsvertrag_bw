8f2752e2df286b627fea92bae1bab9eb8a15048b
Martin Weis <Martin.Weis.newsadress@gmx.de>
2021-05-25 09:22:17 +0200

    extract script added, md2pdf [file] possible

---
+++ extract_keywords.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+srcdir=$(dirname ${BASH_SOURCE[0]})
+srcmd="$srcdir/210506_Koalitionsvertrag_2021-2026.md"
+
+if [ -z "$1" ] ; then
+    echo 'use grep syntax like keyword1\|keyword2, better add single quotes'
+    echo "current source document: $srcmd"
+    echo "no keyword(s) given, exiting."
+    exit 2
+fi
+
+keyletters=$(echo "$1"| sed -e 's/[^a-zA-Z]//g')
+tmpfile=$(mktemp tmp.${keyletters}.$(date "+%Y-%m-%d-%H-%M").XXXX.md)
+
+# grep all titles and keywords, then keep only one before match (max one title), then add blank lines every second line (titles, paragraphs separated)
+grep -i "#\|$keyword" "$srcmd" | grep -i -B 1 "$keyword" | grep -v '^--$' | sed -e 'x;p;x;' > "$tmpfile"
+
+"$srcdir/mdtopdf.sh" "$tmpfile"
+
